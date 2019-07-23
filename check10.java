e867decbed1c104e36ddfe28242e02dfa97e29f0
Shashi kumar verma <43099126+shashi-faced@users.noreply.github.com>
2019-07-24 03:14:12 +0530

    Add files via upload

---
+++ check10.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.*;
+public class check10 {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+		int year = sc.nextInt();
+        boolean leap = false;
+        if(year % 4 == 0)
+        {
+            if( year % 100 == 0)
+            {
+                if ( year % 400 == 0)
+                    leap = true;
+                else
+                    leap = false;
+            }
+            else
+                leap = true;
+        }
+        else
+            leap = false;
+        if(leap)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    }
+}
