7998ee7b266f70d67bfa3057f16e7033ca643e32
Shashi kumar verma <43099126+shashi-faced@users.noreply.github.com>
2019-07-24 02:47:23 +0530

    Add files via upload

---
+++ check7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class check7
+{
+	public static void main(String []args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 0
+		for(int i = 1;i<n;i++)
+			sum = sum + i;
+			System.out.println(sum);
+	}
+}
