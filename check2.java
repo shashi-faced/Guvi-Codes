54933f9b7c364195c61ed3a08d91eb6b8753809a
Shashi kumar verma <43099126+shashi-faced@users.noreply.github.com>
2019-07-23 22:53:36 +0530

    Add files via upload

---
+++ check2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.*;
+
+
+public class check2
+{
+	public static void main(String []args)
+	{
+		Scanner sc = new Scanner(System.in);
+		double num = sc.nextDouble();
+		if(num%2 == 0)
+		{
+			System.out.println("Even");
+		}
+		else if(num%2 == 1)
+		{
+			System.out.println("Odd");
+		}
+		else
+		{
+			System.out.println("invalid");
+		}
+		
+	}
+}
