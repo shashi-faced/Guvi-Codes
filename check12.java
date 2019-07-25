375c7cf71058abf0fae385666dbb8ce46f383c01
Shashi kumar verma <43099126+shashi-faced@users.noreply.github.com>
2019-07-25 19:03:13 +0530

    Add files via upload

---
+++ check12.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class check12
+{
+	public static void main(String []args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int temp = n;
+		int rev = 0;
+		while(n != 0)
+		{
+			int rem = n %10;
+			rev = rev*10 + rem;
+			n = n/10;
+		}
+		if(rev == temp)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}		
+	}
+}
