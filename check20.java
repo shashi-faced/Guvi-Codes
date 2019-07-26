4736743ccbaac77082fa5ab95da219a04c8c23c7
Shashi kumar verma <43099126+shashi-faced@users.noreply.github.com>
2019-07-26 14:32:46 +0530

    Add files via upload

---
+++ check20.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.lang.*;
+public class check20{  
+public static void main(String[] args) 
+{  
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+	StringBuilder sb = new StringBuilder(str);
+// reversing of stringbuilder.
+    System.out.println(sb.reverse());       
+    }  
+} 
