58e975dd596f60f3c66222af1c5e6b89acb0e2d3
Shashi kumar verma <43099126+shashi-faced@users.noreply.github.com>
2019-07-26 11:45:06 +0530

    Add files via upload

---
+++ check17.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class check17
+{
+	public static void main(String []args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 1;i<6;i++)
+		   {
+		   int ins = n*i;
+		    System.out.print(ins+" ");
+	       }	
+	}
+}
