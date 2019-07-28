9195721d6bb0d8a305584a8cfd1d6115877da032
Shashi kumar verma <43099126+shashi-faced@users.noreply.github.com>
2019-07-28 19:58:47 +0530

    Add files via upload

---
+++ check22.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ 
+public class check22{
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String result = "";
+        char firstChar = str.charAt(0);
+        result = result + Character.toUpperCase(firstChar);
+        for (int i = 1; i < str.length(); i++) {
+            char currentChar = str.charAt(i);
+            char previousChar = str.charAt(i - 1);
+            if (previousChar == ' ') {
+                result = result + Character.toUpperCase(currentChar);
+            } else if(previousChar != ' '){
+                result = result + Character.toLowerCase(currentChar);
+            }
+        }
+        System.out.println(result);
+    }
+}
