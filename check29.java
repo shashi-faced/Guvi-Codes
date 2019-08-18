f8fd051264b8b1210b229fc347a8b907295993b4
Shashi kumar verma <43099126+shashi-faced@users.noreply.github.com>
2019-08-18 17:09:15 +0530

    Add files via upload

---
+++ check29.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class check29
+{
+	public static void main(String []args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if(n%2==0)
+		System.out.println("0");
+	else
+		System.out.println("1");
+	}
+}
